b84b479918661aec26b16aec286f87528e7a5750
Manzano46 <kevinmanzano020610@gmail.com>
2024-11-30 15:59:41 -0500

    feat: :sparkles: register page ok

---
+++ frontend/public/config.js
@@ -0,0 +1,14 @@
+const BASE_URL = 'http://127.0.0.1:8000/api/v1';
+
+const USERS_ENDPOINT = `${BASE_URL}/users`;
+
+const LOGIN_PAGE = `/auth/login`;
+const REGISTER_PAGE = `/auth/register`;
+
+
+export {
+  BASE_URL,
+  USERS_ENDPOINT,
+  LOGIN_PAGE,
+  REGISTER_PAGE,
+};
