54abc1b4a7db39f0846930d8e1a7ed280c2b32fa
Manzano46 <kevinmanzano020610@gmail.com>
2024-11-30 20:28:03 -0500

    feat: :sparkles: adding basics events

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -4,6 +4,7 @@ const BASE_URL = 'http://127.0.0.1:8000/';
 const USERS_ENDPOINT = `${BASE_URL_API_V1}users`;
 const REGISTER_ENDPOINT = `${BASE_URL}auth/register`;
 const LOGIN_ENDPOINT = `${BASE_URL}auth/token`;
+const EVENTS_USER = `${BASE_URL_API_V1}events/user/`;
 
 const LOGIN_PAGE = `/`;
 const REGISTER_PAGE = `/auth/register`;
@@ -18,5 +19,6 @@ export {
   REGISTER_PAGE,
   REGISTER_ENDPOINT,
   LOGIN_ENDPOINT,
-  HOME_PAGE
+  HOME_PAGE,
+  EVENTS_USER
 };
